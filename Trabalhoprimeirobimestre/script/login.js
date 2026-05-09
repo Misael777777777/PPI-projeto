005df93921d8e7f858cbe52c34de9581bec45984
Misael777777777 <misael.c@escolar.ifrn.edu.br>
2026-05-09 14:59:02 -0300

    Update login.js

--- Trabalhoprimeirobimestre/script/login.js
+++ Trabalhoprimeirobimestre/script/login.js
@@ -1,14 +1,41 @@
 const usuarios = [
-  { usuario: "mimmarcelo", senha: "Teste123" },
-  { usuario: "lucas", senha: "123" },
-  { usuario: "ryan", senha: "123" },
-  { usuario: "andrei", senha: "123" },
-  { usuario: "neurivan", senha: "123" }
+
+  {
+    usuario: "mimmarcelo",
+    senha: "Teste123"
+  },
+
+  {
+    usuario: "lucas",
+    senha: "123"
+  },
+
+  {
+    usuario: "ryan",
+    senha: "123"
+  },
+
+  {
+    usuario: "andrei",
+    senha: "123"
+  },
+
+  {
+    usuario: "neurivan",
+    senha: "123"
+  }
+
 ];
 
-document
-  .getElementById("btnEntrar")
-  .addEventListener("click", function() {
+const btnEntrar =
+  document.getElementById("btnEntrar");
+
+const btnDev =
+  document.getElementById("btnDev");
+
+btnEntrar.addEventListener(
+  "click",
+  function() {
 
     const usuario =
       document.getElementById("usuario").value;
@@ -17,10 +44,14 @@ document
       document.getElementById("senha").value;
 
     const usuarioValido =
-      usuarios.find(u =>
-        u.usuario === usuario &&
-        u.senha === senha
-      );
+      usuarios.find(function(u) {
+
+        return (
+          u.usuario === usuario &&
+          u.senha === senha
+        );
+
+      });
 
     if (usuarioValido) {
 
@@ -29,21 +60,26 @@ document
         usuario
       );
 
-      window.location.href = "chat.html";
+      window.location.href =
+        "chat.html";
 
     } else {
 
-      alert("Usuário ou senha inválidos!");
+      alert(
+        "Usuário ou senha inválidos!"
+      );
 
     }
 
-  });
+  }
+);
 
-document
-  .getElementById("btnDev")
-  .addEventListener("click", function() {
+btnDev.addEventListener(
+  "click",
+  function() {
 
     window.location.href =
       "desenvolvedores.html";
 
-  });
+  }
+);
